98f5dfbfc235ffbd0f022003e701fa62b49af00c
researcher <researcher>
2024-06-11 11:30:00 +0200

    first commit

---
+++ exifHandler/src/main/java/com/example/exifhandler/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.exifhandler;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        // main function
+    }
+    public static void printLog(String tag, String message){
+        Log.d(tag,message);
+    }
+}
